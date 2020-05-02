7e02a9cf22e39b9675dc4c2d69e32c6444df4b3d
Prince Boamah <princeboamah@Princes-MacBook-Air.local>
2020-05-02 20:58:10 +0000

    A program for numbers

---
+++ num.py
@@ -0,0 +1,38 @@
+# returns an int 
+
+def read_int(prompt):
+    try:
+        prompt_int = int(prompt)
+        break;
+    except ValueError:
+        try:
+            float(prompt)
+            break;
+        except ValueError:
+            print("Please Enter a valid number(integer or a decimal)")
+
+
+# returns a boolean 
+
+def read_boolean(prompt):
+    try:
+        prompt = 'yes'
+        break;
+
+    
+
+
+# arranges the list 
+
+def arrange_nums(num):
+    pass
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
